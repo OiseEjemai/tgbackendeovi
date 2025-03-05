8962f06bce362addea16237ea327a47c2926eb65
CryptoMorki3s <cryptomorki3s@gmail.com>
2025-03-05 17:31:16 +0100

    Bot changes

--- app.js
+++ app.js
@@ -15,22 +15,36 @@ const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 bot.onText(/\/start/, async (msg) => {
   const chatId = msg.chat.id;
 
-  await bot.sendMessage(chatId, "Welcome to Eovi a social media app where users can make post, comment, like and follow other users. For more information, use /help command to know everything about Eovi and to get started.", {
-    reply_markup: {
-      inline_keyboard: [[{ text: "Open Eovi", web_app: { url: FRONTEND_URL } }]],
-    },
-  });
+  await bot.sendMessage(chatId, 
+    "👋 Welcome to *Eovi*! \n\n"
+    + "Eovi is a social media platform where you can *post, comment, like, and follow* other users. \n\n"
+    + "Need help? Use the /help command to learn more about Eovi and how to get started! 🚀", 
+    {
+      reply_markup: {
+        inline_keyboard: [[{ text: "🌐 Open Eovi", web_app: { url: FRONTEND_URL } }]],
+      },
+    }
+  );
 });
 
 bot.onText(/\/help/, async (msg) => {
   const chatId = msg.chat.id;
 
-  await bot.sendMessage(chatId, "Welcome to Eovi, a social media app where users can make post, comment, like and follow other users. \n HOW TO GET STARTED \n  - Sign up to Eovi in other to access its full contents. Tell us information such as your Name, Username, Email, etc \n - That is it, you are good to go. \n To get started, use the /start command.", {
-    // reply_markup: {
-    //   inline_keyboard: [[{ text: "Open App", web_app: { url: FRONTEND_URL } }]],
-    // },
-  });
+  await bot.sendMessage(chatId, 
+    "📌 *Eovi Help Guide* \n\n"
+    + "Eovi is a social media app where you can *post, comment, like, and follow* other users. \n\n"
+    + "🔹 *How to Get Started:* \n"
+    + "1️⃣ *Sign up* for an account to unlock all features. Provide details like *Name, Username, and Email*.\n"
+    + "2️⃣ Once signed up, you’re all set to explore and connect with others!\n\n"
+    + "To start using Eovi, type /start or click the button below. 👇", 
+    {
+      reply_markup: {
+        inline_keyboard: [[{ text: "🚀 Get Started", web_app: { url: FRONTEND_URL } }]],
+      },
+    }
+  );
 });
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
