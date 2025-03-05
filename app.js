a0737842d6fe3e9e912c73138cbc8fc5751d70ed
CryptoMorki3s <cryptomorki3s@gmail.com>
2025-03-05 17:25:42 +0100

    Backend

--- app.js
+++ app.js
@@ -15,10 +15,20 @@ const bot = new TelegramBot(BOT_TOKEN, { polling: true });
 bot.onText(/\/start/, async (msg) => {
   const chatId = msg.chat.id;
 
-  await bot.sendMessage(chatId, "Click below to open the app:", {
+  await bot.sendMessage(chatId, "Welcome to Eovi a social media app where users can make post, comment, like and follow other users. For more information, use /help command to know everything about Eovi and to get started.", {
     reply_markup: {
-      inline_keyboard: [[{ text: "Open App", web_app: { url: FRONTEND_URL } }]],
+      inline_keyboard: [[{ text: "Open Eovi", web_app: { url: FRONTEND_URL } }]],
     },
+  });
+});
+
+bot.onText(/\/help/, async (msg) => {
+  const chatId = msg.chat.id;
+
+  await bot.sendMessage(chatId, "Welcome to Eovi, a social media app where users can make post, comment, like and follow other users. \n HOW TO GET STARTED \n  - Sign up to Eovi in other to access its full contents. Tell us information such as your Name, Username, Email, etc \n - That is it, you are good to go. \n To get started, use the /start command.", {
+    // reply_markup: {
+    //   inline_keyboard: [[{ text: "Open App", web_app: { url: FRONTEND_URL } }]],
+    // },
   });
 });
 
